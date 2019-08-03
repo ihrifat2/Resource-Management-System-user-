5b625e667ebbd3e97da8fa1248912b9f82ac20d0
Imran Hadid <imranhadid03@gmail.com>
2019-08-03 20:28:50 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,190 @@
+<?php
+session_start();
+include 'inc/auth.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>DashBoard</title>
+    <script src="assets/js/jquery.slim.min.js"></script>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="assets/css/all.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <!-- Navbar Start -->
+    <?php include 'inc/navbar.php'; ?>
+    <!-- Navbar End -->
+    
+    <div class="container">
+
+        <!-- Page Heading -->
+        <h1 class="my-4">Your Enrolled Cources
+            <!-- <small>Secondary Text</small -->
+        </h1>
+
+        <div class="row">
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="https://placeimg.com/380/230/nature" alt="">
+                        <span>
+                            <h4>Software Engineering</h4>
+                            <p>SWE111</p>
+                            <p>35 Students</p>
+                        </span>
+                    </div>
+                    <div class="card-desc">
+                        <p>Assignment 1</p>
+                        <a href="course.php" class="btn-card">Read</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="https://placeimg.com/380/230/nature" alt="">
+                        <span>
+                            <h4>Software Engineering</h4>
+                            <p>SWE111</p>
+                            <p>35 Students</p>
+                        </span>
+                    </div>
+                    <div class="card-desc">
+                        <p>Assignment 1</p>
+                        <a href="course.php" class="btn-card">Read</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="https://placeimg.com/380/230/nature" alt="">
+                        <span>
+                            <h4>Software Engineering</h4>
+                            <p>SWE111</p>
+                            <p>35 Students</p>
+                        </span>
+                    </div>
+                    <div class="card-desc">
+                        <p>Assignment 1</p>
+                        <a href="course.php" class="btn-card">Read</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="https://placeimg.com/380/230/nature" alt="">
+                        <span>
+                            <h4>Software Engineering</h4>
+                            <p>SWE111</p>
+                            <p>35 Students</p>
+                        </span>
+                    </div>
+                    <div class="card-desc">
+                        <p>Assignment 1</p>
+                        <a href="course.php" class="btn-card">Read</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="https://placeimg.com/380/230/nature" alt="">
+                        <span>
+                            <h4>Software Engineering</h4>
+                            <p>SWE111</p>
+                            <p>35 Students</p>
+                        </span>
+                    </div>
+                    <div class="card-desc">
+                        <p>Assignment 1</p>
+                        <a href="course.php" class="btn-card">Read</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="https://placeimg.com/380/230/nature" alt="">
+                        <span>
+                            <h4>Software Engineering</h4>
+                            <p>SWE111</p>
+                            <p>35 Students</p>
+                        </span>
+                    </div>
+                    <div class="card-desc">
+                        <p>Assignment 1</p>
+                        <a href="course.php" class="btn-card">Read</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="https://placeimg.com/380/230/nature" alt="">
+                        <span>
+                            <h4>Software Engineering</h4>
+                            <p>SWE111</p>
+                            <p>35 Students</p>
+                        </span>
+                    </div>
+                    <div class="card-desc">
+                        <p>Assignment 1</p>
+                        <a href="course.php" class="btn-card">Read</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="https://placeimg.com/380/230/nature" alt="">
+                        <span>
+                            <h4>Software Engineering</h4>
+                            <p>SWE111</p>
+                            <p>35 Students</p>
+                        </span>
+                    </div>
+                    <div class="card-desc">
+                        <p>Assignment 1</p>
+                        <a href="course.php" class="btn-card">Read</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Pagination -->
+        <!-- <ul class="pagination justify-content-center">
+            <li class="page-item">
+                <a class="page-link" href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                    <span class="sr-only">Previous</span>
+                </a>
+            </li>
+            <li class="page-item">
+                <a class="page-link" href="#">1</a>
+            </li>
+            <li class="page-item">
+                <a class="page-link" href="#">2</a>
+            </li>
+            <li class="page-item">
+                <a class="page-link" href="#">3</a>
+            </li>
+            <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </li>
+        </ul> -->
+
+    </div>
+</body>
+</html>
+<?php
+include 'inc/notification.php';
+?>

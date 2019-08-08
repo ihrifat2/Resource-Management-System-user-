f87f3ead2f57e772b1e3bcc52f255938d424b89f
Imran Hadid <imranhadid03@gmail.com>
2019-08-08 19:24:18 +0600

    Update helper.php

--- helper.php
+++ helper.php
@@ -1,5 +1,5 @@
 <?php
-
+//get first word of a string
 function getFirstWord($value) {
 	$firstWord = "";
 	$firstCharacter = explode(" ", $value);
@@ -9,5 +9,36 @@ function getFirstWord($value) {
 	}
 	return $firstWord;
 }
+//count time 
+function momentAgo($start, $expired) {
+    $startDate = new DateTime($start);
+    $expirDate = new DateTime($expired);
+    $interval = $startDate->diff($expirDate);
+    $year   = $interval->format('%Y'); 
+    $month  = $interval->format('%m'); 
+    $day    = $interval->format('%d');
+    if($year!=0){ 
+        $ago = $year.' year(s) ago'; 
+    }elseif($month!=0){ 
+        $ago = $month.' month(s) ago'; 
+    } else { 
+        $ago = $day.' day(s) ago'; 
+    }
+    return $ago;
+}
+//get random number
+function createRandomPassword() { 
+    $chars = "abcdefghijkmnopqrstuvwxyz0123456789"; 
+    srand((double)microtime()*1000000); 
+    $i = 0; 
+    $pass = '' ; 
+    while ($i <= 7) { 
+        $num = rand() % 33; 
+        $tmp = substr($chars, $num, 1); 
+        $pass = $pass . $tmp; 
+        $i++; 
+    } 
+    return $pass;
+} 
 
 ?>
